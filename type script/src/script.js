a7cd105e8d5742b84d60027759256757e10d7eb5
Yunus <152853532+YunusSotiboldiyev@users.noreply.github.com>
2024-10-16 15:40:53 +0500

    script.js

--- type script/src/script.js
+++ type script/src/script.js
@@ -1,10 +1,35 @@
-function doubleValue(value) {
-    if (value === null) {
-        console.log("Xato");
+var myUser = { name: "Alice" };
+var myAdmin = { role: "Moderator" };
+function getUserInfo(person) {
+    if ("name" in person) {
+        return person.name;
+    }
+    else if ("role" in person) {
+        return person.role;
+    }
+    return '';
+}
+console.log(getUserInfo(myUser));
+console.log(getUserInfo(myAdmin));
+//2-vazifa
+function processArray(arr) {
+    if (typeof arr === "string") {
+        return arr.map(function (str) { return str.toUpperCase(); });
+    }
+    else if (typeof arr === "number") {
+        return arr.map(function (num) { return num * 2; });
+    }
+    return [];
+}
+console.log(processArray([1]));
+//3-vazifa
+function returnValue(flag) {
+    if (flag) {
+        return 42;
     }
     else {
-        var result = value * 2;
-        console.log(result);
+        return "Hello";
     }
 }
-doubleValue(5);
+console.log(returnValue(true));
+console.log(returnValue(false));
